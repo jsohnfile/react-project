741f2a63667355e0fe55d9f75f2b78c2c428a120
jsohnfile <hoodieee@Jaes-MacBook-Air.local>
2020-09-29 11:43:09 -0600

    create sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Sidebar.css';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/* Twitter icon */}
+            <TwitterIcon />
+
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+        </div>
+    )
+}
+
+export default Sidebar
