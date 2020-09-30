75a48ed4a3b8d123058e236fc213935d09e23b89
jsohnfile <hoodieee@Jaes-MacBook-Air.local>
2020-09-30 14:28:48 -0600

    post updated

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TweetBox from "./TweetBox";
+import Post from "./Post";
 import "./Feed.css";
 
 function Feed() {
@@ -13,6 +14,7 @@ function Feed() {
             {/* TweetBox */}
             <TweetBox />
             {/* Posts */}
+            <Post/>
         </div>
     )
 }
